7009d76296d3d07e1f7b4499a19d749b85d56755
Andreas Gruhler <andreas.gruhler@adfinis.com>
2020-07-20 23:37:23 +0200

    use goreleaser install script

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ curl -L -o ~/${go_version}.tar.gz https://dl.google.com/go/${go_version}.tar.gz
 tar -C /usr/local -xzf ~/${go_version}.tar.gz
 mkdir /root/go
 
+mkdir -p /root/.ssh
 echo "GOPATH=/root/go" >>/root/.ssh/environment
 echo "PATH=$PATH:/usr/local/go/bin:/root/go/bin" >>/root/.ssh/environment
 echo "PermitUserEnvironment yes" >>/etc/ssh/sshd_config
@@ -14,4 +15,4 @@ echo "PermitUserEnvironment yes" >>/etc/ssh/sshd_config
 mkdir -p ${GOPATH}/src/github.com/hashicorp
 git clone --branch ${nomad_version} https://github.com/hashicorp/nomad.git ${GOPATH}/src/github.com/hashicorp/nomad
 
-go get github.com/goreleaser/goreleaser
+curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh
